89f33cc650f1e11afc47c133a14b7b357ed09d1f
david620 <davidacosta309@outlook.com>
2018-05-17 12:19:55 -0400

    Add files via upload

---
+++ RYW-039/RYW-039.ino
@@ -0,0 +1,26 @@
+
+int ledc = 13;  
+int sensorPin = 0;
+int period = 100;
+int change, oldvalue, value;
+void setup ()
+{
+  pinMode(ledc, OUTPUT);
+  Serial.begin (9600);
+}
+void loop ()
+{
+  value = analogRead (sensorPin);
+  change = abs(oldvalue - value);
+  if(change >= 2 )
+  {
+    Serial.println (change);
+    analogWrite(ledc, 0); 
+  }
+  else
+  { 
+    analogWrite(ledc,255); 
+  }
+  oldvalue = value;
+  delay (period);
+}
